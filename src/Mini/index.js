04e19408cec29aba1aa422c718bd50472df2615a
Zack Yang <zackyang@outlook.com>
2016-10-04 11:19:24 +0800

    fix(Mini): add PropTypes import.

--- src/Mini/index.js
+++ src/Mini/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
 import styles from './styles';
 
